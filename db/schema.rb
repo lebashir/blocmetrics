43bf5506da61c9849117c550f28a4692de9383f5
lebashir <bashir.abihabib@gmail.com>
2017-08-10 21:14:12 -0500

    generated registed application

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170810230206) do
+ActiveRecord::Schema.define(version: 20170811005853) do
+
+  create_table "applications", force: :cascade do |t|
+    t.string   "name"
+    t.string   "url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
